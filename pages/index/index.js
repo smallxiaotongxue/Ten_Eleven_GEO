26396deb5c106d5107fdce7a523b31715ddb875d
smallxiaotongxue <583883731@qq.com>
2019-06-18 15:27:45 +0800

    首页修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,18 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ]
   },
   //事件处理函数
   bindViewTap: function() {
