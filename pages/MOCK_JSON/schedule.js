5138aef201bcea79e323204f49d9dd350c5c3737
smallxiaotongxue <583883731@qq.com>
2019-06-30 22:09:45 +0800

    demo

---
+++ pages/MOCK_JSON/schedule.js
@@ -0,0 +1,212 @@
+export const CLASS_INFO_LIST = [
+  {
+    name: "周1课程信息",
+    maxClass: 12,
+    infos: [
+      {
+        name: "地理",
+        background: "#3388FF",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "08:10",
+        endTime: "09:10",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "09:10",
+        endTime: "10:10",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "10:10",
+        endTime: "11:10",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "11:10",
+        endTime: "12:10",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "13:10",
+        endTime: "14:10",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "14:10",
+        endTime: "15:10",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "15:10",
+        endTime: "16:10",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "17:10",
+        endTime: "18:10",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "18:10",
+        endTime: "19:10",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "20:10",
+        endTime: "21:10",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "21:10",
+        endTime: "22:10",
+        hidden: "false"
+      }
+    ]
+  },
+  {
+    name: "周2课程信息",
+    maxClass: 8,
+    infos: [
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "08:00",
+        endTime: "09:00",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "08:00",
+        endTime: "09:00",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "08:00",
+        endTime: "09:00",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "08:00",
+        endTime: "09:00",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "08:00",
+        endTime: "09:00",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "08:00",
+        endTime: "09:00",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "08:00",
+        endTime: "09:00",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "08:00",
+        endTime: "09:00",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "08:00",
+        endTime: "09:00",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "08:00",
+        endTime: "09:00",
+        hidden: "false"
+      },
+      {
+        name: "",
+        background: "#eeeeee",
+        classRoomNum: "",
+        teacher: "",
+        startTime: "08:00",
+        endTime: "09:00",
+        hidden: "false"
+      }
+    ]
+  }
+];
